12eb24361b645dcb7f80198beef552de47af1b0f
wax911 <wax911@users.noreply.github.com>
2024-11-24 11:57:44 +0000

    chore(automation): auto format code using deno fmt
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/series/service/skyhook/types.ts
+++ src/series/service/skyhook/types.ts
@@ -1,6 +1,8 @@
 import { EpisodeModel, SkyhookModel } from './remote/types.ts';
 import { Show } from './transformer/types.ts';
 
-export type SkyhookEpisode = EpisodeModel
+export type SkyhookEpisode = EpisodeModel;
 
-export type SkyhookShow = Omit<SkyhookModel, 'firstAired' | 'lastUpdated'> & Show
+export type SkyhookShow =
+  & Omit<SkyhookModel, 'firstAired' | 'lastUpdated'>
+  & Show;
